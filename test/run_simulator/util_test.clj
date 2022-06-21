0ff4a76c7e89a032c96f9bd4f29415478e0d461f
Dan Fornika <dfornika@gmail.com>
2022-06-21 14:20:08 -0700

    Add more util tests

--- test/run_simulator/util_test.clj
+++ test/run_simulator/util_test.clj
@@ -5,6 +5,30 @@
             [clojure.spec.test.alpha :as stest]))
 
 (t/deftest randomly-select-unit
+  (t/testing "Empty collection returns nil"
+    (t/is (nil? (util/randomly-select []))))
+  (t/testing "One-element collection always returns same element."
+    (t/is (= :a (util/randomly-select [:a]))))
   (t/testing "Selected element is member of input collection"
-    (t/is (contains? #{1 2 3 4 5} (util/randomly-select [1 2 3 4 5]))))
+    (let [coll #{1 2 3 4 5}]
+      (t/is (contains? coll (util/randomly-select (vec coll))))))
   )
+
+
+(t/deftest int->well-unit
+  (t/testing "0 => A01"
+    (t/is (= "A01" (util/int->well 0))))
+  (t/testing "95 => H12"
+    (t/is (= "H12" (util/int->well 95))))
+  (t/testing "96 => A01"
+    (t/is (= "A01" (util/int->well 96))))
+  )
+
+
+(t/deftest now!-unit
+  (t/testing "Timestamp matches regex"
+    (t/is (not (nil? (re-matches #"\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d+-\d{2}:\d{2}" (util/now!))))))
+  )
+          
+          
+
