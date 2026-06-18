2aabe947e7b023f4e6a9450e7491542588a87dd5
Dan Fornika <dfornika@gmail.com>
2026-06-18 13:59:00 -0700

    Add user namespace

---
+++ dev/user.clj
@@ -0,0 +1,27 @@
+(ns user
+  (:require [clj-reload.core :as reload]
+            [run-simulator.core :as core]
+            [run-simulator.util :as util]))
+
+(reload/init
+  {:dirs ["src" "dev" "test"]})
+
+(comment
+  (reload/reload)
+  ,)
+
+(defonce db (atom {}))
+
+(comment
+
+  (let [config (util/load-edn! "dev-config.edn")]
+    (swap! db assoc :config config)
+    (swap! db assoc :current-run-num-by-instrument-id
+           (into {} (map (juxt :instrument-id :starting-run-number) (get-in @db [:config :instruments]))))
+    (swap! db assoc :current-date (util/iso-date-str->date (get-in @db [:config :starting-date])))
+    (swap! db assoc :current-plate-number (get-in @db [:config :starting-plate-number])))
+
+  (core/simulate-run! db {:instrument-type :i100})
+  (core/simulate-run! db {:instrument-type :miseq})
+  (core/simulate-run! db {:instrument-type :nextseq})
+  ,)
